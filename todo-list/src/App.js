7c1fa4b5a7b6aac45e1025a4184eee11da1dad62
cypark <blackco66@gmail.com>
2020-08-19 20:33:35 +0900

    velopert todolist semi-done

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+class App extends Component {
+  id = 3;
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false },
+    ],
+  };
+  handleChange = (e) => {
+    console.log('change');
+    this.setState({
+      input: e.target.value,
+    });
+  };
+  handleCreate = () => {
+    console.log('create');
+    const { input, todos } = this.state;
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false,
+      }),
+    });
+  };
+  handleKeyPress = (e) => {
+    console.log('keypress');
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  };
+  handleToggle = (id) => {
+    const { todos } = this.state;
+    const index = todos.findIndex((todo) => todo.id === id);
+    const selected = todos[index];
+    const nextTodos = [...todos];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked,
+    };
+    console.log('toggle');
+    this.setState({
+      todos: nextTodos,
+    });
+  };
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    console.log('remove');
+    this.setState({
+      todos: todos.filter((todo) => todo.id !== id),
+    });
+  };
+  render() {
+    const { input, todos } = this.state;
+    const { handleChange, handleCreate, handleKeyPress } = this;
+    console.log('app');
+    return (
+      <TodoListTemplate
+        form={
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        }
+      >
+        <TodoItemList
+          todos={todos}
+          onToggle={this.handleToggle}
+          onRemove={this.handleRemove}
+        />
+      </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
---
+++ todo-list/src/components/TodoItemList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+    console.log('todoitemlist');
+    const todoList = todos.map(({ id, text, checked }) => (
+      <TodoItem
+        id={id}
+        text={text}
+        checked={checked}
+        onToggle={onToggle}
+        onRemove={onRemove}
+        key={id}
+      />
+    ));
+    return <div>{todoList}</div>;
+  }
+}
+
+export default TodoItemList;
